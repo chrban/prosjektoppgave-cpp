da08c2251350dcfd57ea95465f1859032ff76bd1
chrban <christer@bang.is>
2015-04-28 23:06:20 +0200

    OMG

--- build-test4-Desktop_Qt_5_4_1_clang_64bit-Debug/moc_game.cpp
+++ build-test4-Desktop_Qt_5_4_1_clang_64bit-Debug/moc_game.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_game_t {
-    QByteArrayData data[5];
-    char stringdata[39];
+    QByteArrayData data[7];
+    char stringdata[68];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,10 +33,13 @@ QT_MOC_LITERAL(0, 0, 4), // "game"
 QT_MOC_LITERAL(1, 5, 5), // "setUp"
 QT_MOC_LITERAL(2, 11, 0), // ""
 QT_MOC_LITERAL(3, 12, 13), // "pickedUpLinus"
-QT_MOC_LITERAL(4, 26, 12) // "showMainMenu"
+QT_MOC_LITERAL(4, 26, 12), // "showMainMenu"
+QT_MOC_LITERAL(5, 39, 14), // "showKillScreen"
+QT_MOC_LITERAL(6, 54, 13) // "showSuperBoss"
 
     },
-    "game\0setUp\0\0pickedUpLinus\0showMainMenu"
+    "game\0setUp\0\0pickedUpLinus\0showMainMenu\0"
+    "showKillScreen\0showSuperBoss"
 };
 #undef QT_MOC_LITERAL
 
@@ -46,7 +49,7 @@ static const uint qt_meta_data_game[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -54,11 +57,15 @@ static const uint qt_meta_data_game[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   29,    2, 0x0a /* Public */,
-       3,    0,   30,    2, 0x0a /* Public */,
-       4,    0,   31,    2, 0x0a /* Public */,
+       1,    0,   39,    2, 0x0a /* Public */,
+       3,    0,   40,    2, 0x0a /* Public */,
+       4,    0,   41,    2, 0x0a /* Public */,
+       5,    0,   42,    2, 0x0a /* Public */,
+       6,    0,   43,    2, 0x0a /* Public */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -74,6 +81,8 @@ void game::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void *
         case 0: _t->setUp(); break;
         case 1: _t->pickedUpLinus(); break;
         case 2: _t->showMainMenu(); break;
+        case 3: _t->showKillScreen(); break;
+        case 4: _t->showSuperBoss(); break;
         default: ;
         }
     }
@@ -105,13 +114,13 @@ int game::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 5;
     }
     return _id;
 }
