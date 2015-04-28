8ac931f191ca13d51f45dc8f285de8e89d6db8c3
chrban <christer@bang.is>
2015-04-28 23:04:13 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 9b64fd52c1b5c699e3ec573697241c6fb7d52b56, reversing
    changes made to 4e7580d1a7800ad3a55a568456178e23c5733076.

--- test4/levelfactory.cpp
+++ test4/levelfactory.cpp
@@ -4,6 +4,7 @@
 #include <utility>
 #include <map>
 #include "figur.h"
+#include "game.h"
 
 #include <iostream>
 #include <iterator>
@@ -12,51 +13,145 @@
 #include <QFile>
 #include <QMessageBox>
 #include <QPair>
-#include <QRegExp>
 
-LevelFactory::LevelFactory() {}
 
-void LevelFactory::loadMap( QGraphicsScene* scene, int f  )
+int LevelFactory::x[20]={100,130,160,190,220,250,280,310,340,370,460,400,600,600,600,600,600,166,460,400};
+int LevelFactory::y[20]={480,480,480,480,480,480,480,480,480,480,450,380,525,500,475,450,425,442,412,342};
+
+std::vector< std::pair<int,int> > parVect;
+std::vector<  std::pair<int, std::pair<int,int>  > > superVect; // tanken er: framenumber, x,y koordinater
+
+
+
+int bokser[50];
+//std::vector<int> bokser;
+//extern game * g;
+
+void LevelFactory::readMap()
 {
-int frameLokal=0;
-
+    int frameIndex=0;
+    int antallFrames=0;
     QFile file(":/new/img/map.txt");
     if(!file.open(QIODevice::ReadOnly)) {
         qDebug()<<"noe gikk galt ved lesing av fil";
         //TODO: Hva skal throwes?
     }
+
     QTextStream in(&file);
 
     while(!in.atEnd()) {
-        QGraphicsItem* item;
         QString line = in.readLine();
+        if(line != "-"){
+            QStringList listeTest = line.split(",");
 
-            QStringList maplist = line.split(",");
+//            QString last = line.split(",").takeLast();
 
-            if(maplist[0]=="frame")
-            {
-                frameLokal = maplist[1].toInt();
-            }
-            else if(frameLokal == f )
-            {
-            if(maplist[0]=="box")
-                item = BF->mekk(maplist[1].toInt(),maplist[2].toInt()) ;
-            if(maplist[0]=="linus")
-                item = LF->mekk(maplist[1].toInt(),maplist[2].toInt()) ;
-            if(maplist[0]=="ground")
-                item = GF->mekk(maplist[1].toInt(),maplist[2].toInt()) ;
-            if(maplist[0]=="cloud")
-                item = CF->mekk(maplist[1].toInt(),maplist[2].toInt()) ;
+//            qDebug()<<"first"<<first<<"last"<<last << "line er: "<<line;
+//            superVect.push_back( std::make_pair( frameIndex,std::make_pair(first.toInt(),last.toInt())));
+            superVect.push_back( std::make_pair( frameIndex ,std::make_pair( listeTest[0].toInt() , listeTest[1].toInt() )));
+            antallFrames++;
 
-            scene->addItem(item);
-            }
+
+        }
+        else
+        {
+
+            bokser[frameIndex]=antallFrames;
+
+//            qDebug()<<"trffet - i map.txt, øker frameIndex til "<<frameIndex<<"bokser[getFrame()]="<<bokser[0]<<"antFrames"<<antallFrames;
+            antallFrames=0;
+            frameIndex++;
+
+        }
     }
     file.close();
 }
 
 
+//denne metoden skal sende ut riktige koordinater til boksene som skal tegnes basert på hvilken frame av banen man er på
+std::pair<int, int> LevelFactory::frameHandler(std::vector<  std::pair<int, std::pair<int,int>  > > f )
+{
 
 
 
+//        qDebug()<<"getFrame er"<< getFrame()<<"f[bokser[getFrame()]]"<<f[bokser[getFrame()]].first ;
+
+
+//   for(auto i : superVect)  // mhm kan bruke noe i denne duren ja
+//   {
+//       if( i.first == 0)
+//           return i.second;
+//   }
+
+
+    for(int i = bokser[0 /*getFrame() når jeg får det til å fungere...*/ ]; i != 0;i--)
+    {
+        if(f[i].first == 0 )
+        {
+                    return f[bokser[getFrame()]--].second;
+        }
+
+    }
+
+}
+
+int LevelFactory::getFrame()
+{
+    return frame;
+}
+
+
+void LevelFactory::increaseFrame()
+{
+    qDebug()<<"------------------FRAME var.."<<frame<<"men getFrame er:"<<getFrame();
+    frame++;
+    qDebug()<<"------------------FRAME ØKES TIL"<<frame;
+}
+
+void LevelFactory::decreaseFrame()
+{
+    frame--;
+}
+
+bool LevelFactory::framesLeft()
+{
+    if( bokser[getFrame()]==0 )
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+        qDebug()<<"boolen er tydligvis falsk";
+    }
+}
+
+
+
+LevelFactory::LevelFactory() : count(0), reading(true)
+{
+    qDebug()<<"FØR  Frame initializes  " << frame;
+    frame = 0;
+    qDebug()<<"Frame initializes til " << frame;
+}
+
+int LevelFactory::getNextY()
+{
+
+    return y[count++];
+}
+
+int LevelFactory::getNextX()
+{
+    return x[count];
+
+}
+
+std::pair<int,int> LevelFactory::getCoordinates()
+{
+//    qDebug()<<"kaller koordinater"<<getFrame()<<bokser[getFrame()];
+    return frameHandler(superVect);
+
+}
 
 
